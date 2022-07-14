c7b7202b96a496b4899f68fd13f36a57be9764d0
magsther <55060295+magsther@users.noreply.github.com>
2022-07-14 19:35:38 +0200

    Create variables.tf

---
+++ 03-elastic-cloud/modules/elastic-cloud/variables.tf
@@ -0,0 +1,29 @@
+variable "ec_instance_name" {
+  type        = string
+  default     = ""
+  description = "Name of Elastic deployment"
+}
+
+variable "region" {
+  type        = string
+  default     = "azure-westeurope"
+  description = "Region of Elastic deployment"
+}
+
+variable "es_version" {
+  type        = string
+  default     = "8.2.3"
+  description = "Version of Elastic deployment"
+}
+
+variable "deployment_template_id" {
+  type        = string
+  default     = "azure-memory-optimized"
+  description = "Azure SKU of Elastic deployment"
+}
+
+variable "autoscale" {
+  default     = false
+  type        = bool
+  description = "Autocale enabled of Elastic deployment"
+}
