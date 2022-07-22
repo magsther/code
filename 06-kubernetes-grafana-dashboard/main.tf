27ed4e922f4439bf4d05e5f603463673c45c2eac
magsther <55060295+magsther@users.noreply.github.com>
2022-07-22 20:00:49 +0200

    Create main.tf

---
+++ 06-kubernetes-grafana-dashboard/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "0.0.13"
+    }
+    ec = {
+      source  = "elastic/ec"
+      version = "~> 0.4.1"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name           = "kindcluster"
+  node_image     = "kindest/node:v1.24.0"
+  wait_for_ready = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+
+    node {
+      role = "worker"
+    }
+  }
+}
+
+module "kube" {
+  source       = "./modules/kube-prometheus"
+  kube-version = "36.2.0"
+}
+
+module "elastic" {
+  source     = "./modules/elastic-cloud"
+  ec_api_key = var.ec_api_key
+}
